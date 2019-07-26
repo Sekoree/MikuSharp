eb376ec2d42a19e7d292f16e8f779cc5dc23deb5
Speyd3r <speyd3r@srgg.de>
2019-07-26 13:33:42 +0200

    Fixed all Weeb commands

---
+++ MikuSharp/Entities/MeekMoe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MikuSharp.Entities
+{
+    public class MeekMoe
+    {
+        public string url { get; set; }
+        public string creator { get; set; }
+    }
+}
