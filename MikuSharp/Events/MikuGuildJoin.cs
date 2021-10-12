35983e2d37ab11f86b72a7b8668502d23a0a4fa2
Lala Sabathil <aiko@aiko-it-systems.eu>
2021-10-12 15:24:16 +0200

    Add debug functions on-the-fly and mikuguild join event handler

---
+++ MikuSharp/Events/MikuGuildJoin.cs
@@ -0,0 +1,50 @@
+﻿using DisCatSharp;
+using DisCatSharp.Entities;
+using DisCatSharp.EventArgs;
+
+using Microsoft.Extensions.Logging;
+
+using MikuSharp.Enums;
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MikuSharp.Events
+{
+    /// <summary>
+    /// Event handler for the miku guild.
+    /// </summary>
+    public class MikuGuild
+    {
+        /// <summary>
+        /// Fired when a new guild member joins.
+        /// </summary>
+        /// <param name="sender">The client.</param>
+        /// <param name="args">The event args.</param>
+        public static async Task OnJoinAsync(DiscordClient sender, GuildMemberAddEventArgs args)
+        {
+            await Task.FromResult(true);
+        }
+
+        /// <summary>
+        /// Fired when a guild member is updated.
+        /// </summary>
+        /// <param name="sender">The discord client.</param>
+        /// <param name="args">The event args.</param>
+        public static async Task OnUpdateAsync(DiscordClient sender, GuildMemberUpdateEventArgs args)
+        {
+            if (args.PendingBefore.HasValue && args.PendingBefore == true)
+            {
+                if (args.PendingAfter.HasValue && args.PendingAfter == false)
+                {
+                    ulong member_role_id = 483280207927574528;
+                    var member_role = args.Guild.GetRole(member_role_id);
+                    await args.Member.GrantRoleAsync(member_role);
+                }
+            }
+            await Task.FromResult(true);
+        }
+    }
+}
