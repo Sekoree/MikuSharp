d447e5a5d6d8721871e307c758cf4e1574d32980
Lala Sabathil <aiko@aitsys.dev>
2023-05-28 15:23:15 +0200

    refactor: ndd

--- MikuSharp/Utilities/NND.cs
+++ MikuSharp/Utilities/NND.cs
@@ -15,31 +15,42 @@ public static class NND
 			var linux_exe = "nndownload.py";
 			string cmd = download_exe;
 			await ctx.EditFollowupAsync(msg_id, new DiscordWebhookBuilder().WithContent("Downloading video (this may take up to 10 min)"));
+
 			if (OperatingSystem.IsLinux())
 				cmd = linux_exe;
-			Process downloadProcess = new();
-			downloadProcess.StartInfo.FileName = cmd;
-			downloadProcess.StartInfo.Arguments = $"-g -o {$@"{s}"}.mp4 {$@"{n}"}";
-			downloadProcess.OutputDataReceived += (d, f) =>
+
+			using (Process downloadProcess = new())
 			{
-				ctx.Client.Logger.LogDebug("{data}", $"\n{f.Data}\n");
-			};
-			downloadProcess.Start();
-			await downloadProcess.WaitForExitAsync();
+				downloadProcess.StartInfo.FileName = cmd;
+				downloadProcess.StartInfo.Arguments = $"-g -o {$@"{s}"}.mp4 {$@"{n}"}";
+				downloadProcess.OutputDataReceived += (d, f) =>
+				{
+					ctx.Client.Logger.LogDebug("{data}", $"\n{f.Data}\n");
+				};
+
+				downloadProcess.Start();
+				await downloadProcess.WaitForExitAsync();
+			}
+
 			var songTitle = videoPage?.Video?.Title ?? "[NND] Unknown Title";
 			var songArtist = videoPage?.Owner?.Nickname ?? "Unknown Artist";
 			await ctx.EditFollowupAsync(msg_id, new DiscordWebhookBuilder().WithContent("Converting"));
-			Process convertProgress = new();
-			convertProgress.StartInfo.FileName = "ffmpeg";
-			convertProgress.StartInfo.Arguments = $"-i {$@"{s}"}.mp4 -metadata title=\"{songTitle}\" -metadata artist=\"{songArtist}\" {$@"{s}"}.mp3";
-			convertProgress.OutputDataReceived += (d, f) =>
+
+			using (Process convertProcess = new())
 			{
-				ctx.Client.Logger.LogDebug("{data}", f.Data);
-			};
-			convertProgress.Start();
-			await convertProgress.WaitForExitAsync();
-			File.Delete($@"{s}.mp4");
+				convertProcess.StartInfo.FileName = "ffmpeg";
+				convertProcess.StartInfo.Arguments = $"-i {$@"{s}"}.mp4 -metadata title=\"{songTitle}\" -metadata artist=\"{songArtist}\" {$@"{s}"}.mp3";
+				convertProcess.OutputDataReceived += (d, f) =>
+				{
+					ctx.Client.Logger.LogDebug("{data}", f.Data);
+				};
+
+				convertProcess.Start();
+				await convertProcess.WaitForExitAsync();
+			}
+
 			MemoryStream ms = new(await File.ReadAllBytesAsync($@"{s}.mp3"));
+			File.Delete($@"{s}.mp4");
 			File.Delete($@"{s}.mp3");
 			ms.Position = 0;
 			return ms;
