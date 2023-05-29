99507f6be427bd7482080a56f4406124f5251805
Lala Sabathil <aiko@aitsys.dev>
2023-05-29 10:48:42 +0200

    refactor: web

--- MikuSharp/Utilities/Web.cs
+++ MikuSharp/Utilities/Web.cs
@@ -9,15 +9,18 @@ public static class Web
 	public static async Task<Nekos_Life> GetNekosLifeAsync(this HttpClient client, string url)
 	{
 		var dl = JsonConvert.DeserializeObject<Nekos_Life>(await client.GetStringAsync(url));
-		MemoryStream str = new(await client.GetByteArrayAsync(Other.resizeLink(dl.Url)))
+		var imgBytes = await client.GetByteArrayAsync(Other.resizeLink(dl.Url));
+		var str = new MemoryStream(imgBytes)
 		{
 			Position = 0
 		};
 		dl.Data = str;
 		dl.Filetype = MimeGuesser.GuessExtension(str);
-		var em = new DiscordEmbedBuilder();
-		em.WithImageUrl($"attachment://image.{dl.Filetype}");
-		em.WithFooter("by nekos.life");
+		var em = new DiscordEmbedBuilder
+		{
+			ImageUrl = $"attachment://image.{dl.Filetype}",
+			Footer = new() { Text = "by nekos.life" }
+		};
 		dl.Embed = em.Build();
 		return dl;
 	}
@@ -26,12 +29,18 @@ public static class Web
 	{
 		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", MikuBot.Config.KsoftSiToken);
 		var v = JsonConvert.DeserializeObject<KsoftSiRanImg>(await client.GetStringAsync("https://api.ksoft.si/images/random-image?tag=hentai_gif&nsfw=true"));
-		MemoryStream img = new(await client.GetByteArrayAsync(Other.resizeLink(v.url)));
+		var imgBytes = await client.GetByteArrayAsync(Other.resizeLink(v.url));
+		var img = new MemoryStream(imgBytes)
+		{
+			Position = 0
+		};
 		v.Data = img;
 		v.Filetype = MimeGuesser.GuessExtension(img);
-		var em = new DiscordEmbedBuilder();
-		em.WithImageUrl($"attachment://image.{v.Filetype}");
-		em.WithFooter("by KSoft.si");
+		var em = new DiscordEmbedBuilder
+		{
+			ImageUrl = $"attachment://image.{v.Filetype}",
+			Footer = new() { Text = "by KSoft.si" }
+		};
 		v.Embed = em.Build();
 		return v;
 	}
@@ -39,15 +48,18 @@ public static class Web
 	public static async Task<NekoBot> GetNekobotAsync(this HttpClient client, string url)
 	{
 		var dl = JsonConvert.DeserializeObject<NekoBot>(await client.GetStringAsync(url));
-		MemoryStream str = new(await client.GetByteArrayAsync(Other.resizeLink(dl.message)))
+		var imgBytes = await client.GetByteArrayAsync(Other.resizeLink(dl.message));
+		var str = new MemoryStream(imgBytes)
 		{
 			Position = 0
 		};
 		dl.Data = str;
 		dl.Filetype = MimeGuesser.GuessExtension(str);
-		var em = new DiscordEmbedBuilder();
-		em.WithImageUrl($"attachment://image.{dl.Filetype}");
-		em.WithFooter("by nekobot.xyz");
+		var em = new DiscordEmbedBuilder
+		{
+			ImageUrl = $"attachment://image.{dl.Filetype}",
+			Footer = new() { Text = "by nekobot.xyz" }
+		};
 		dl.Embed = em.Build();
 		return dl;
 	}
@@ -55,13 +67,16 @@ public static class Web
 	public static async Task<WeebSh> GetWeebShAsync(this HttpClient client, string query, string[] tags = null, NsfwSearch nsfw = NsfwSearch.False)
 	{
 		var weeurl = await MikuBot.WeebClient.GetRandomAsync(query, tags, nsfw: nsfw);
-		MemoryStream img = new(await client.GetByteArrayAsync(weeurl.Url))
+		var imgBytes = await client.GetByteArrayAsync(weeurl.Url);
+		var img = new MemoryStream(imgBytes)
 		{
 			Position = 0
 		};
-		var em = new DiscordEmbedBuilder();
-		em.WithImageUrl($"attachment://image.{MimeGuesser.GuessExtension(img)}");
-		em.WithFooter("by weeb.sh");
+		var em = new DiscordEmbedBuilder
+		{
+			ImageUrl = $"attachment://image.{MimeGuesser.GuessExtension(img)}",
+			Footer = new() { Text = "by weeb.sh" }
+		};
 		return new WeebSh
 		{
 			ImgData = img,
