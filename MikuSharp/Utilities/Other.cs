ea490a9f3e0f7c83ed865626ce3591c348d2787f
Lala Sabathil <aiko@aitsys.dev>
2023-05-29 10:32:43 +0200

    refactor: other

--- MikuSharp/Utilities/Other.cs
+++ MikuSharp/Utilities/Other.cs
@@ -3,17 +3,13 @@
 public static class Other
 {
 	public static string resizeLink(string url)
-	{
-		return $"https://api.meek.moe/im/?image={url}&resize=500";
-	}
+		=> $"https://api.meek.moe/im/?image={url}&resize=500";
 
 	public static async Task DeferAsync(this InteractionContext ctx, bool ephemeral = true)
 	{
 		var builder = new DiscordInteractionResponseBuilder();
 		if (ephemeral)
-		{
 			builder.AsEphemeral();
-		}
 		await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, builder);
 	}
 }
