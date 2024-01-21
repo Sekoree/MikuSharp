fe7236cc1e07fbd5efab2170f37a6fc668867650
Lala Sabathil <aiko@aitsys.dev>
2024-01-21 17:52:20 +0100

    fix: ping in followup instead

--- MikuSharp/Commands/Action.cs
+++ MikuSharp/Commands/Action.cs
@@ -21,12 +21,13 @@ internal class Action : ApplicationCommandsModule
 	{
 		await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"{ctx.User.Mention} hugs {user.Mention} uwu"));
 		var wsh = await ctx.Client.RestClient.GetWeebShAsync("hug", new[] { "" });
+		wsh.Embed.WithDescription($"{ctx.User.Mention} hugs {user.Mention} uwu");
 
 		DiscordWebhookBuilder builder = new();
-		builder.WithContent($"{ctx.User.Mention} hugs {user.Mention} uwu");
 		builder.AddFile($"image.{wsh.Extension}", wsh.ImgData);
 		builder.AddEmbed(wsh.Embed.Build());
 		await ctx.EditResponseAsync(builder);
+		await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(user.Mention).AddMention(new UserMention(user)));
 	}
 
 	[SlashCommand("kiss", "Kiss someone!")]
@@ -34,12 +35,13 @@ internal class Action : ApplicationCommandsModule
 	{
 		await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"{ctx.User.Mention} kisses {user.Mention} >~<"));
 		var wsh = await ctx.Client.RestClient.GetWeebShAsync("kiss", new[] { "" });
+		wsh.Embed.WithDescription($"{ctx.User.Mention} kisses {user.Mention} >~<");
 
 		DiscordWebhookBuilder builder = new();
-		builder.WithContent($"{ctx.User.Mention} kisses {user.Mention} >~<");
 		builder.AddFile($"image.{wsh.Extension}", wsh.ImgData);
 		builder.AddEmbed(wsh.Embed.Build());
 		await ctx.EditResponseAsync(builder);
+		await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(user.Mention).AddMention(new UserMention(user)));
 	}
 
 	[SlashCommand("lick", "Lick someone!")]
@@ -47,12 +49,13 @@ internal class Action : ApplicationCommandsModule
 	{
 		await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"{ctx.User.Mention} licks {user.Mention} owo"));
 		var wsh = await ctx.Client.RestClient.GetWeebShAsync("lick", new[] { "" });
+		wsh.Embed.WithDescription($"{ctx.User.Mention} licks {user.Mention} owo");
 
 		DiscordWebhookBuilder builder = new();
-		builder.WithContent($"{ctx.User.Mention} licks {user.Mention} owo");
 		builder.AddFile($"image.{wsh.Extension}", wsh.ImgData);
 		builder.AddEmbed(wsh.Embed.Build());
 		await ctx.EditResponseAsync(builder);
+		await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(user.Mention).AddMention(new UserMention(user)));
 	}
 
 	[SlashCommand("pat", "Pat someone!")]
@@ -62,14 +65,15 @@ internal class Action : ApplicationCommandsModule
 		var weeurl = await MikuBot.WeebClient.GetRandomAsync("pat", new[] { "" });
 		Stream img = new MemoryStream(await ctx.Client.RestClient.GetByteArrayAsync(Other.ResizeLink(weeurl.Url)));
 		var em = new DiscordEmbedBuilder();
+		em.WithDescription($"{ctx.User.Mention} pats {user.Mention} #w#");
 		em.WithImageUrl($"attachment://image.{MimeGuesser.GuessExtension(img)}");
 		em.WithFooter("by nekos.life");
 
 		DiscordWebhookBuilder builder = new();
-		builder.WithContent($"{ctx.User.Mention} pats {user.Mention} #w#");
 		builder.AddFile($"image.{MimeGuesser.GuessExtension(img)}", img);
 		builder.AddEmbed(em.Build());
 		await ctx.EditResponseAsync(builder);
+		await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(user.Mention).AddMention(new UserMention(user)));
 	}
 
 	[SlashCommand("poke", "Poke someone!")]
@@ -79,14 +83,15 @@ internal class Action : ApplicationCommandsModule
 		var weeurl = await MikuBot.WeebClient.GetRandomAsync("poke", new[] { "" });
 		Stream img = new MemoryStream(await ctx.Client.RestClient.GetByteArrayAsync(Other.ResizeLink(weeurl.Url)));
 		var em = new DiscordEmbedBuilder();
+		em.WithDescription($"{ctx.User.Mention} pokes {user.Mention} ÓwÒ");
 		em.WithImageUrl($"attachment://image.{MimeGuesser.GuessExtension(img)}");
 		em.WithFooter("by nekos.life");
 
 		DiscordWebhookBuilder builder = new();
-		builder.WithContent($"{ctx.User.Mention} pokes {user.Mention} ÓwÒ");
 		builder.AddFile($"image.{MimeGuesser.GuessExtension(img)}", img);
 		builder.AddEmbed(em.Build());
 		await ctx.EditResponseAsync(builder);
+		await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(user.Mention).AddMention(new UserMention(user)));
 	}
 
 	[SlashCommand("slap", "Slap someone!")]
@@ -96,13 +101,14 @@ internal class Action : ApplicationCommandsModule
 		var weeurl = await MikuBot.WeebClient.GetRandomAsync("slap", new[] { "" });
 		Stream img = new MemoryStream(await ctx.Client.RestClient.GetByteArrayAsync(Other.ResizeLink(weeurl.Url)));
 		var em = new DiscordEmbedBuilder();
+		em.WithDescription($"{ctx.User.Mention} slaps {user.Mention} ÒwÓ");
 		em.WithImageUrl($"attachment://image.{MimeGuesser.GuessExtension(img)}");
 		em.WithFooter("by nekos.life");
 
 		DiscordWebhookBuilder builder = new();
-		builder.WithContent($"{ctx.User.Mention} slaps {user.Mention} ÒwÓ");
 		builder.AddFile($"image.{MimeGuesser.GuessExtension(img)}", img);
 		builder.AddEmbed(em.Build());
 		await ctx.EditResponseAsync(builder);
+		await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent(user.Mention).AddMention(new UserMention(user)));
 	}
 }
